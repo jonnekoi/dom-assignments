a24df2d77c1f3e64da29311e0c2be26b7ad4e096
jonnekoi <jonnekoivisto00@gmail.com>
2024-03-27 09:42:55 +0200

    commit

---
+++ t1/t1.js
@@ -0,0 +1,43 @@
+// array for todo list
+const todoList = [
+  {
+    id: 1,
+    task: 'Learn HTML',
+    completed: true,
+  },
+  {
+    id: 2,
+    task: 'Learn CSS',
+    completed: true,
+  },
+  {
+    id: 3,
+    task: 'Learn JS',
+    completed: false,
+  },
+  {
+    id: 4,
+    task: 'Learn TypeScript',
+    completed: false,
+  },
+  {
+    id: 5,
+    task: 'Learn React',
+    completed: false,
+  },
+];
+
+const list = document.querySelector('ul');
+
+for (const todo of todoList) {
+  let checked = '';
+  if (todo.completed) {
+    checked = 'checked';
+  }
+  const html = `
+  <li>
+     <input type="checkbox" id="todo-${todo.id}" ${checked}>
+     <label for="todo-${todo.id}">${todo.task}</label>
+  </li>`;
+  list.insertAdjacentHTML('beforeend', html);
+}
